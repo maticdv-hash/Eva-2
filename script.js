e4ba07c5a4931cccf4887ec8a41cd281fa0f000a
maticdv-hash <mati.carrascodv@gmail.com>
2026-05-25 20:27:13 -0400

    Captura de campos del formulario

--- script.js
+++ script.js
@@ -2,3 +2,8 @@
 let colaboradores = [];
 
 const formulario = document.getElementById("formulario");
+
+const nombreInput = document.getElementById("nombre");
+const apellidoInput = document.getElementById("apellido");
+const cargoInput = document.getElementById("cargo");
+const correoInput = document.getElementById("correo");
